ef2f087cee1de4f06ae9ec53c5491d6a1fe5c3a2
liliang14 <liliang14@tal.com>
2022-02-16 18:06:48 +0800

    feat: list

---
+++ src/components/toDoList/listItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="item">
+    <span>{{objData.name}}</span>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    objData: {
+      type: [Object],
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 4em;
+  background-color: #f5f5f5;
+}
+</style>
--- src/components/toDoList/toDoList.vue
+++ src/components/toDoList/toDoList.vue
@@ -3,22 +3,95 @@
     <back />
     <h1>ToDo List</h1>
     <h1>{{test}}</h1>
+    <div
+      id="list"
+      class="clearfix"
+    >
+      <div
+        class="list-group"
+        v-for="(item, index) in listData"
+        :key="index"
+      >
+        <div class="list-title">{{item.title}}</div>
+        <draggable
+          class="draggable"
+          :list="item.list"
+          :group="item.title"
+        >
+          <transition-group
+            @end="drag = false"
+            @start="drag = true"
+            :name="!drag ? 'flip-list' : null"
+          >
+            <div
+              class="draggable-item"
+              v-for="element in item.list"
+              :key="element.id"
+            >
+              <list-item :objData="element"></list-item>
+            </div>
+          </transition-group>
+        </draggable>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import back from "@/utils/backBtn/back";
+import ListItem from "./listItem";
+import Draggable from "vuedraggable";
 
 export default {
   name: "UpperCase",
   data() {
     return {
       test: null,
+      listData: [
+        {
+          title: "进行中",
+          list: [
+            {
+              id: 1,
+              name: "测试1",
+            },
+
+            {
+              id: 2,
+              name: "测试2",
+            },
+
+            {
+              id: 3,
+              name: "测试3",
+            },
+
+            {
+              id: 4,
+              name: "测试4",
+            },
+            {
+              id: 5,
+              name: "测试5",
+            },
+            {
+              id: 6,
+              name: "测试6",
+            },
+          ],
+        },
+        {
+          title: "已完成",
+          list: [],
+        },
+      ],
     };
   },
   props: {},
   components: {
     back,
+    ListItem,
+    Draggable,
   },
   watch: {},
   mounted() {
@@ -42,8 +115,41 @@ export default {
   flex-direction: column;
   align-items: center;
   height: 100%;
+  width: 100%;
+  overflow-x: auto;
 }
 h1 {
   cursor: default;
+}
+
+#list {
+  height: 100%;
+  width: 100%;
+  max-width: 24em;
+  padding: 2em 0;
+  .list-group {
+    width: 100%;
+    height: 100%;
+    min-height: 10em;
+  }
+  .list-title {
+    font-size: 1em;
+    font-weight: 700;
+    margin-bottom: 1em;
+  }
+  .draggable {
+    height: 100%;
+    flex: 1;
+  }
+  .draggable-item {
+    margin-bottom: 0.5em;
+    cursor: pointer;
+  }
+  .flip-list-move {
+    transition: transform 0.5s;
+  }
+  .no-move {
+    transition: transform 0s;
+  }
 }
 </style>
